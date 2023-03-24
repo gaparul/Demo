14813aa017a72a08c01c3f6e310301835590208d
Parul <parulghotikar@gmail.com>
2023-03-24 19:26:06 +0530

    imoported lombom.*

--- src/main/java/com/demo/paymentsdemo/entity/Student.java
+++ src/main/java/com/demo/paymentsdemo/entity/Student.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import java.util.Set;
 
 @Getter
